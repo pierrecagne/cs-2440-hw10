b050e7407fdd766c76a8a4772fc9ad7fbb754883
Remousamavi <remousamavi@gmail.com>
2025-11-14 11:46:09 -0500

    Assignments completed

--- Tree.java
+++ Tree.java
@@ -1,32 +1,117 @@
 public class Tree<T> {
-    // TODO: Implement the unique field
+    TreeNode<T> root;
 
+    /**
+     * New Tree.
+     */
     public Tree() {
-        // TODO: Initialize the tree
+        this.root = null;
     }
 
+    /**
+     * New Tree with defined root.
+     * @param root The TreeNode to set as the root.
+     */
     public Tree(TreeNode<T> root) {
-        // TODO: Initialize the tree with a root
+        setRoot(root);
     }
 
+    /**
+     * Set the Tree's root.
+     * @param root The root TreeNode to set.
+     */
     public void setRoot(TreeNode<T> root) {
-        // TODO: Set the root node
+        this.root = root;
     }
 
+    /**
+     * Get the current root.
+     * @return
+     */
     public TreeNode<T> getRoot() {
-        // TODO: Return the root node
-        return null; // For compiling purposes only. TODO: Replace by adequate return
+        return this.root;
     }
 
+    /**
+     * Returns a breadth-first list of items in a given tree.
+     * @return The breadth-first list.
+     */
     public List<T> breadthFirstSearch() {
-        // TODO: Implement BFS
-        // Don't forget to transform your collection R into a list at the end (if it is not already)
-        return null; // For compiling purposes only. TODO: Replace by adequate return
+        List<T> R = new List<T>();
+        if (this.root == null)
+            return R;
+        Queue<TreeNode<T>> C = new Queue<TreeNode<T>>();
+        C.enqueue(this.root);
+        while (!C.isEmpty()) {
+            TreeNode<T> rem = C.dequeue();
+            R.addAtEnd(rem.getValue());
+            for (TreeNode<T> t : rem.children) {
+                C.enqueue(t);
+            }
+        }
+        return R;
     }
 
+    /**
+     * Returns a depth-first list of items in a given tree.
+     * @return The depth-first list.
+     */
     public List<T> depthFirstSearch() {
-        // TODO: Implement DFS
-        // Don't forget to transform your collection R into a list at the end (if it is not already)
-        return null; // For compiling purposes only. TODO: Replace by adequate return
+        List<T> R = new List<T>();
+        if (this.root == null)
+            return R;
+        Stack<TreeNode<T>> C = new Stack<TreeNode<T>>();
+        C.push(this.root);
+        while (!C.isEmpty()) {
+            TreeNode<T> rem = C.pop();
+            R.addAtEnd(rem.getValue());
+            for (TreeNode<T> t : rem.children) {
+                C.push(t);
+            }
+        }
+        return R;
+    }
+
+    /**
+     * Return a string representation of a list.
+     * @param list The list to parse.
+     * @return The string out.
+     */
+    public static String printList(List list) {
+        String out = "(";
+        for (int i = 0; i < list.length(); i++) {
+            out += list.atPosition(i);
+            out += i < list.length() - 1 ? ", " : ")";
+        }
+        return out;
+    }
+
+    /**
+     * Test it all.
+     * @param args Console args.
+     */
+    public static void main(String[] args) {
+        TreeNode<Integer> A = new TreeNode<Integer>(1);
+        TreeNode<Integer> B = new TreeNode<Integer>(2);
+        TreeNode<Integer> C = new TreeNode<Integer>(3);
+        TreeNode<Integer> D = new TreeNode<Integer>(4);
+        TreeNode<Integer> E = new TreeNode<Integer>(5);
+        TreeNode<Integer> F = new TreeNode<Integer>(6);
+        TreeNode<Integer> G = new TreeNode<Integer>(7);
+        TreeNode<Integer> H = new TreeNode<Integer>(8);
+        TreeNode<Integer> I = new TreeNode<Integer>(9);
+        Tree<Integer> tree = new Tree<Integer>(A);
+        A.addChild(B);
+        A.addChild(C);
+        A.addChild(D);
+        B.addChild(E);
+        B.addChild(F);
+        D.addChild(G);
+        D.addChild(H);
+        D.addChild(I);
+        List<Integer> breadthList = tree.breadthFirstSearch();
+        System.out.println(printList(breadthList));
+        List<Integer> depthList = tree.depthFirstSearch();
+        System.out.println(printList(depthList));
     }
 }
--- TreeNode.java
+++ TreeNode.java
@@ -1,25 +1,21 @@
 public class TreeNode<T> {
-    // TODO: Implement the two fields
+    T value;
+    List<TreeNode<T>> children;
 
     public TreeNode(T value) {
-        // TODO: Initialize the node
-        // The resulting node contains `value` as data
-        // and have an empty list of children.
+        this.value = value;
+        this.children = new List<TreeNode<T>>();
     }
 
     public void addChild(TreeNode<T> child) {
-        // TODO: Add a child to this node
-        // You can choose *where* to add the child
-        // but try to opt for the most time-efficient.
+        this.children.addOnHead(child);
     }
 
     public List<TreeNode<T>> getChildren() {
-        // TODO: Return the list of children
-        return null; // For compiling purposes only. TODO: Replace by adequate return
+        return this.children;
     }
 
     public T getValue() {
-        // TODO: Return the node's value
-        return null; // For compiling purposes only. TODO: Replace by adequate return
+        return this.value;
     }
 }
